da282436f6918fbd8ad3587ccaecbe5cb031ac84
bernard <bernard>
2010-10-08 15:05:10 +0000

    honour Evert Combe
    git-svn-id: svn://svn.hercules-390.org/hercules/trunk@6820 969364d3-6d58-4e41-9446-b7e21338ea1b

--- crypto/dyncrypt.c
+++ crypto/dyncrypt.c
@@ -676,6 +676,7 @@ void power(unsigned char *a, unsigned char b)
 
 /*----------------------------------------------------------------------------*/
 /* Program to generate exp_table                                              */
+/* Many thanks to Evert Combe                                                 */
 /*----------------------------------------------------------------------------*/
 int main(void)
 {
