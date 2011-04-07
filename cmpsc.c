7ef21abb49e19cc0fac1a8998eb425c31b443c73
bernard <bernard>
2011-04-07 07:44:46 +0000

    CMPSC has been debugged together with Bob. It has shown that the expansion stopped at the point where an expansion entry was fetched. hercules 3.05 worked fine. It showed that the address was calculated with the MADDR macro, BUT the ADDRESS_MAXWRAP was not used. It was in the old days. I hope that the error is fixed now.
    git-svn-id: svn://svn.hercules-390.org/hercules/trunk@7400 969364d3-6d58-4e41-9446-b7e21338ea1b
    git-svn-id: svn://svn.hercules-390.org/hercules/trunk@7401 969364d3-6d58-4e41-9446-b7e21338ea1b

--- cmpsc.c
+++ cmpsc.c
@@ -544,7 +544,7 @@ static BYTE *ARCH_DEP(fetch_cce)(struct cc *cc, unsigned index)
 
   index *= 8;
   if(unlikely(!cc->dict[index / 0x800]))
-    cc->dict[index / 0x800] = MADDR(cc->dictor + (index / 0x800) * 0x800, cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey);
+    cc->dict[index / 0x800] = MADDR((cc->dictor + (index / 0x800) * 0x800) & ADDRESS_MAXWRAP(cc->regs), cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey);
   cce = &cc->dict[index / 0x800][index % 0x800];
   ITIMER_SYNC(cc->dictor + index, 8 - 1, cc->regs);
 
@@ -858,7 +858,7 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
     /* Get the sibling descriptor */
     index = (CCE_cptr(cc->cce) + sd_ptr) * 8;
     if(unlikely(!cc->dict[index / 0x800]))
-      cc->dict[index / 0x800] = MADDR(cc->dictor + (index / 0x800) * 0x800, cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey);
+      cc->dict[index / 0x800] = MADDR((cc->dictor + (index / 0x800) * 0x800) & ADDRESS_MAXWRAP(cc->regs), cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey);
     sd1 = &cc->dict[index / 0x800][index % 0x800];
     ITIMER_SYNC(cc->dictor + index, 8 - 1, cc->regs);
 
@@ -866,7 +866,7 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
     if(cc->f1)
     {
       if(unlikely(!cc->edict[index / 0x800]))
-        cc->edict[index / 0x800] = MADDR(cc->dictor + cc->dctsz + (index / 0x800) * 0x800, cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey); 
+        cc->edict[index / 0x800] = MADDR((cc->dictor + cc->dctsz + (index / 0x800) * 0x800) & ADDRESS_MAXWRAP(cc->regs), cc->r2, cc->regs, ACCTYPE_READ, cc->regs->psw.pkey); 
       sd2 = &cc->edict[index / 0x800][index % 0x800];
       ITIMER_SYNC(cc->dictor + cc->dctsz + index, 8 - 1, cc->regs);
 
@@ -1396,7 +1396,7 @@ static void ARCH_DEP(expand_is)(struct ec *ec, U16 is)
   /* Get expansion character entry */
   index = is * 8;
   if(unlikely(!ec->dict[index / 0x800]))
-    ec->dict[index / 0x800] = MADDR(ec->dictor + (index / 0x800) * 0x800, ec->r2, ec->regs, ACCTYPE_READ, ec->regs->psw.pkey);
+    ec->dict[index / 0x800] = MADDR((ec->dictor + (index / 0x800) * 0x800) & ADDRESS_MAXWRAP(ec->regs), ec->r2, ec->regs, ACCTYPE_READ, ec->regs->psw.pkey);
   ece = &ec->dict[index / 0x800][index % 0x800];
   ITIMER_SYNC(ec->dictor + index, 8 - 1, ec->regs);
 
@@ -1425,7 +1425,7 @@ static void ARCH_DEP(expand_is)(struct ec *ec, U16 is)
     /* Get preceding entry */
     index = ECE_pptr(ece) * 8;
     if(unlikely(!ec->dict[index / 0x800]))
-      ec->dict[index / 0x800] = MADDR(ec->dictor + (index / 0x800) * 0x800, ec->r2, ec->regs, ACCTYPE_READ, ec->regs->psw.pkey);	  
+      ec->dict[index / 0x800] = MADDR((ec->dictor + (index / 0x800) * 0x800) & ADDRESS_MAXWRAP(ec->regs), ec->r2, ec->regs, ACCTYPE_READ, ec->regs->psw.pkey);	  
     ece = &ec->dict[index / 0x800][index % 0x800];
     ITIMER_SYNC(ec->dictor + index, 8 - 1, ec->regs);
 
