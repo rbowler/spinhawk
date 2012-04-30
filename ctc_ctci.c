7c0f15e8e22d52697011200ae3322d3d238f52b4
John P. Hartmann <jphartmann@gmail.com>
2012-04-30 11:33:00 +0200

    Using pre-configured tunnel device with CTCI
    This patch avoids the use of a setuid program to configure a
    tun/tap device on unix.  Instead, the device can be defined and
    configured at system boot.
    
    The patch allows for just a single parameter on a CTCI device, the
    name of the tunnel device, which must be created and configured
    elsewhere, for example by openvpn.
    
    The original two-parameter code is still in place and active.
    
    The devices in the configuration file then look like this:
    
    03e0.2  ctci    tun0
    
    I have this in rc.local to create the tunnel device:
    
    openvpn --mktun --dev tun0 --group staff
    ifconfig tun0 192.168.2.65 pointopoint 10.0.0.33
    echo 1 >/proc/sys/net/ipv4/ip_forward
    
    I also have this udev rule, but I am not convinced it is still
    necessary as permissions are now more relaxed than they were when I
    started with Hercules (or openvpn has been fixed in the meantime).
    
    KERNEL=="tun",                  GROUP="staff", MODE="0660"

--- ctc_ctci.c
+++ ctc_ctci.c
@@ -155,6 +155,7 @@ int  CTCI_Init( DEVBLK* pDEVBLK, int argc, char *argv[] )
     int             nIFType;            // Interface type
     int             nIFFlags;           // Interface flags
     char            thread_name[32];    // CTCI_ReadThread
+    int             preconfigured = 0;  // Preconfigured net device
 
     nIFType =               // Interface type
         0
@@ -253,6 +254,7 @@ int  CTCI_Init( DEVBLK* pDEVBLK, int argc, char *argv[] )
              pDevCTCBLK->szTUNCharName,
      sizeof( pDevCTCBLK->pDEVBLK[1]->filename ) );
 
+    if (pDevCTCBLK->szTUNDevName[0]) preconfigured = 1;
     rc = TUNTAP_CreateInterface( pDevCTCBLK->szTUNCharName,
                                  IFF_TUN | IFF_NO_PI,
                                  &pDevCTCBLK->fd,
@@ -271,86 +273,90 @@ int  CTCI_Init( DEVBLK* pDEVBLK, int argc, char *argv[] )
                   pDevCTCBLK->szTUNDevName);
     }
 
+    if (!preconfigured)
+    {
 #if defined(OPTION_W32_CTCI)
 
-    // Set the specified driver/dll i/o buffer sizes..
-    {
-        struct tt32ctl tt32ctl;
-
-        memset( &tt32ctl, 0, sizeof(tt32ctl) );
-        strlcpy( tt32ctl.tt32ctl_name, pDevCTCBLK->szTUNDevName, sizeof(tt32ctl.tt32ctl_name) );
-
-        tt32ctl.tt32ctl_devbuffsize = pDevCTCBLK->iKernBuff;
-        if( TUNTAP_IOCtl( pDevCTCBLK->fd, TT32SDEVBUFF, (char*)&tt32ctl ) != 0  )
+        // Set the specified driver/dll i/o buffer sizes..
         {
-            logmsg( _("HHCCT074W TT32SDEVBUFF failed for device %s: %s.\n"),
-                    pDevCTCBLK->szTUNDevName, strerror( errno ) );
-        }
+            struct tt32ctl tt32ctl;
 
-        tt32ctl.tt32ctl_iobuffsize = pDevCTCBLK->iIOBuff;
-        if( TUNTAP_IOCtl( pDevCTCBLK->fd, TT32SIOBUFF, (char*)&tt32ctl ) != 0  )
-        {
-            logmsg( _("HHCCT075W TT32SIOBUFF failed for device %s: %s.\n"),
-                    pDevCTCBLK->szTUNDevName, strerror( errno ) );
+            memset( &tt32ctl, 0, sizeof(tt32ctl) );
+            strlcpy( tt32ctl.tt32ctl_name, pDevCTCBLK->szTUNDevName, sizeof(tt32ctl.tt32ctl_name) );
+
+            tt32ctl.tt32ctl_devbuffsize = pDevCTCBLK->iKernBuff;
+            if( TUNTAP_IOCtl( pDevCTCBLK->fd, TT32SDEVBUFF, (char*)&tt32ctl ) != 0  )
+            {
+                logmsg( _("HHCCT074W TT32SDEVBUFF failed for device %s: %s.\n"),
+                        pDevCTCBLK->szTUNDevName, strerror( errno ) );
+            }
+
+            tt32ctl.tt32ctl_iobuffsize = pDevCTCBLK->iIOBuff;
+            if( TUNTAP_IOCtl( pDevCTCBLK->fd, TT32SIOBUFF, (char*)&tt32ctl ) != 0  )
+            {
+                logmsg( _("HHCCT075W TT32SIOBUFF failed for device %s: %s.\n"),
+                        pDevCTCBLK->szTUNDevName, strerror( errno ) );
+            }
         }
-    }
 #endif
 
 #ifdef OPTION_TUNTAP_CLRIPADDR
-    VERIFY( TUNTAP_ClrIPAddr ( pDevCTCBLK->szTUNDevName ) == 0 );
+        VERIFY( TUNTAP_ClrIPAddr ( pDevCTCBLK->szTUNDevName ) == 0 );
 #endif
 
 #ifdef OPTION_TUNTAP_SETMACADDR
 
-    if( !pDevCTCBLK->szMACAddress[0] )   // (if MAC address unspecified)
-    {
-        in_addr_t  wrk_guest_ip_addr;
-        MAC        wrk_guest_mac_addr;
-
-        if ((in_addr_t)-1 != (wrk_guest_ip_addr = inet_addr( pDevCTCBLK->szGuestIPAddr )))
+        if( !pDevCTCBLK->szMACAddress[0] )   // (if MAC address unspecified)
         {
-            build_herc_iface_mac ( wrk_guest_mac_addr, (const BYTE*) &wrk_guest_ip_addr );
+            in_addr_t  wrk_guest_ip_addr;
+            MAC        wrk_guest_mac_addr;
 
-            snprintf
-            (
-                pDevCTCBLK->szMACAddress,  sizeof( pDevCTCBLK->szMACAddress ),
+            if ((in_addr_t)-1 != (wrk_guest_ip_addr = inet_addr( pDevCTCBLK->szGuestIPAddr )))
+            {
+                build_herc_iface_mac ( wrk_guest_mac_addr, (const BYTE*) &wrk_guest_ip_addr );
 
-                "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X"
+                snprintf
+                (
+                    pDevCTCBLK->szMACAddress,  sizeof( pDevCTCBLK->szMACAddress ),
 
-                ,wrk_guest_mac_addr[0]
-                ,wrk_guest_mac_addr[1]
-                ,wrk_guest_mac_addr[2]
-                ,wrk_guest_mac_addr[3]
-                ,wrk_guest_mac_addr[4]
-                ,wrk_guest_mac_addr[5]
-            );
+                    "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X"
+
+                    ,wrk_guest_mac_addr[0]
+                    ,wrk_guest_mac_addr[1]
+                    ,wrk_guest_mac_addr[2]
+                    ,wrk_guest_mac_addr[3]
+                    ,wrk_guest_mac_addr[4]
+                    ,wrk_guest_mac_addr[5]
+                );
+            }
         }
-    }
 
-    TRACE
-    (
-        "** CTCI_Init: %4.4X (%s): IP \"%s\"  -->  default MAC \"%s\"\n"
+        TRACE
+        (
+            "** CTCI_Init: %4.4X (%s): IP \"%s\"  -->  default MAC \"%s\"\n"
 
-        ,pDevCTCBLK->pDEVBLK[0]->devnum
-        ,pDevCTCBLK->szTUNDevName
-        ,pDevCTCBLK->szGuestIPAddr
-        ,pDevCTCBLK->szMACAddress
-    );
+            ,pDevCTCBLK->pDEVBLK[0]->devnum
+            ,pDevCTCBLK->szTUNDevName
+            ,pDevCTCBLK->szGuestIPAddr
+            ,pDevCTCBLK->szMACAddress
+        );
 
-    VERIFY( TUNTAP_SetMACAddr ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szMACAddress  ) == 0 );
+        VERIFY( TUNTAP_SetMACAddr ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szMACAddress  ) == 0 );
 #endif
 
-    VERIFY( TUNTAP_SetIPAddr  ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szDriveIPAddr ) == 0 );
+        VERIFY( TUNTAP_SetIPAddr  ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szDriveIPAddr ) == 0 );
 
-    VERIFY( TUNTAP_SetDestAddr( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szGuestIPAddr ) == 0 );
+        VERIFY( TUNTAP_SetDestAddr( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szGuestIPAddr ) == 0 );
 
 #ifdef OPTION_TUNTAP_SETNETMASK
-    VERIFY( TUNTAP_SetNetMask ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szNetMask     ) == 0 );
+        VERIFY( TUNTAP_SetNetMask ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szNetMask     ) == 0 );
 #endif
 
-    VERIFY( TUNTAP_SetMTU     ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szMTU         ) == 0 );
+        VERIFY( TUNTAP_SetMTU     ( pDevCTCBLK->szTUNDevName, pDevCTCBLK->szMTU         ) == 0 );
 
-    VERIFY( TUNTAP_SetFlags   ( pDevCTCBLK->szTUNDevName, nIFFlags                  ) == 0 );
+        VERIFY( TUNTAP_SetFlags   ( pDevCTCBLK->szTUNDevName, nIFFlags                  ) == 0 );
+
+    }
 
     // Copy the fd to make panel.c happy
     pDevCTCBLK->pDEVBLK[0]->fd =
@@ -1185,7 +1191,7 @@ static int  ParseArgs( DEVBLK* pDEVBLK, PCTCBLK pCTCBLK,
     OPTRESET();
     optind      = 0;
     // Check for correct number of arguments
-    if( argc < 2 )
+    if( argc < 1 )
     {
         logmsg( _("HHCCT056E %4.4X: Incorrect number of parameters\n"),
                pDEVBLK->devnum );
@@ -1379,37 +1385,45 @@ static int  ParseArgs( DEVBLK* pDEVBLK, PCTCBLK pCTCBLK,
 
     if( !pCTCBLK->fOldFormat )
     {
-        // New format has 2 and only 2 parameters (Though several options).
-        if( argc != 2 )
+        if (1 == argc)                /* Pre-configured net device   */
         {
-            logmsg( _("HHCCT057E %4.4X: Incorrect number of parameters\n"),
-                pDEVBLK->devnum );
-            return -1;
+            strlcpy(pCTCBLK->szTUNDevName, argv[0], sizeof(pCTCBLK->szTUNDevName));
+            argc--; argv++;
         }
-
-        // Guest IP Address
-        if( inet_aton( *argv, &addr ) == 0 )
+        else
         {
-            logmsg( _("HHCCT058E %4.4X: Invalid IP address %s\n"),
-                pDEVBLK->devnum, *argv );
-            return -1;
+            // New format has 2 and only 2 parameters (Though several options).
+            if( argc != 2 )
+            {
+                logmsg( _("HHCCT057E %4.4X: Incorrect number of parameters\n"),
+                    pDEVBLK->devnum );
+                return -1;
+            }
+
+            // Guest IP Address
+            if( inet_aton( *argv, &addr ) == 0 )
+            {
+                logmsg( _("HHCCT058E %4.4X: Invalid IP address %s\n"),
+                    pDEVBLK->devnum, *argv );
+                return -1;
+            }
+
+            strcpy( pCTCBLK->szGuestIPAddr, *argv );
+
+            argc--; argv++;
+
+            // Driver IP Address
+            if( inet_aton( *argv, &addr ) == 0 )
+            {
+                logmsg( _("HHCCT059E %4.4X: Invalid IP address %s\n"),
+                    pDEVBLK->devnum, *argv );
+                return -1;
+            }
+
+            strcpy( pCTCBLK->szDriveIPAddr, *argv );
+
+            argc--; argv++;
         }
-
-        strcpy( pCTCBLK->szGuestIPAddr, *argv );
-
-        argc--; argv++;
-
-        // Driver IP Address
-        if( inet_aton( *argv, &addr ) == 0 )
-        {
-            logmsg( _("HHCCT059E %4.4X: Invalid IP address %s\n"),
-                pDEVBLK->devnum, *argv );
-            return -1;
-        }
-
-        strcpy( pCTCBLK->szDriveIPAddr, *argv );
-
-        argc--; argv++;
     }
     else // if( pCTCBLK->fOldFormat )
     {
